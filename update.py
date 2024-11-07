9d5d92204f1a71d3a0670edf1558689da4ed0ae9
ankudovychm <ankudovych.m@northeastern.edu>
2024-11-06 19:53:44 -0500

    Create update.py

---
+++ update.py
@@ -0,0 +1,12 @@
+import main as main
+
+hist_csv_name = "historic.csv"
+teams_csv_name = 'teams.csv'
+
+
+all_data = main.CurrentResults(teams_csv_name, hist_csv_name, "20242025")
+
+
+main.macguffin(all_data,"data/macguffin.csv")
+main.ReverseMacguffin(all_data,"data/reverse-macguffin.csv")
+main.all_conf(all_data, teams_csv_name,"data/ConfMcguffins.csv")
