92604ce1344cd6c35f03896c663c1502f17baef7
ankudovychm <ankudovych.m@northeastern.edu>
2024-11-07 11:36:40 -0500

    Adding leaderboard to nav bar

--- docs/latest-leaderboard.js
+++ docs/latest-leaderboard.js
@@ -1,40 +1,39 @@
 // URLs to the MacGuffin and Reverse MacGuffin CSVs in the GitHub repository
 var FRCSV = "https://raw.githubusercontent.com/ankudovychm/collegehockey-macguffin/main/data/leaderboard/forwardranges.csv";
 var FTCSV = "https://raw.githubusercontent.com/ankudovychm/collegehockey-macguffin/main/data/leaderboard/forwardtotals.csv";
+
 var RRCSV = "https://raw.githubusercontent.com/ankudovychm/collegehockey-macguffin/main/data/leaderboard/reverseranges.csv";
 var RTCSV = "https://raw.githubusercontent.com/ankudovychm/collegehockey-macguffin/main/data/leaderboard/reversetotals.csv";
 ;
 
 
 // Function to fetch and display the latest entry from a CSV
-function fetchLatestEntry(url, elementId, label) {
+function fetchLatestEntry(url, elementId) {
     Papa.parse(url, {
         download: true,
         header: true,
         complete: function(results) {
             var data = results.data;
-            if (data.length > 0) {
+            
                 // Get the last entry (latest)
-                var FirsttEntry = data[1];
+                var FirsttEntry = data[0];
 
                 // Extract Date and Team values, and use default values if missing
-                var len = FirsttEntry["Length (in days)"] || "unknown len";
+                var len = FirsttEntry["Length (in days)"] || FirsttEntry["Total Length (in days)"];
                 var team = FirsttEntry.Team || "unknown team";
                 
                 // Display the latest entry in the specified element
-                document.getElementById(elementId).innerHTML = `${label} <span class="bold-text">${team}</span>, for ${len} days.`;
+                document.getElementById(elementId).innerHTML = `<span class="bold-text">${team}</span>, for ${len} days.`;
 
-            } else {
-                document.getElementById(elementId).textContent = `No data available for ${label.toLowerCase()}.`;
-            }
         },
-        error: function() {
-            document.getElementById(elementId).textContent = `Error loading ${label.toLowerCase()}.`;
-        }
     });
 }
 
 // Load the latest MacGuffin and Reverse MacGuffin data on page load
 document.addEventListener("DOMContentLoaded", () => {
-    fetchLatestEntry(FRCSV, "forward-consec", "");
+    fetchLatestEntry(FRCSV, "forward-consec");
+    fetchLatestEntry(FTCSV, "forward-all");
+    fetchLatestEntry(RRCSV, "reverse-consec");
+    fetchLatestEntry(RTCSV, "reverse-all");
 });
+
